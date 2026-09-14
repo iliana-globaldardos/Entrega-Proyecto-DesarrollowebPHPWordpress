71c6b899b18186ed7d9af2086fb9251ef96710d4
Global Dardos <iliana@globaldardos.com.ar>
2026-09-14 12:45:05 -0300

    modificacion de imagenes

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css">
-    <title>Entrega Intermedia</title>
+    <title>Actividad Integradora Obligatoria Módulo 1</title>
 </head>
 <body>
     <div class="card">
-        <h1>Entrega intermedia</h1>
+        <h1>Formulario con metodo GET</h1>
 
         <!--FORMULARIO CON EL METODO GET-->
         <form method="get">
